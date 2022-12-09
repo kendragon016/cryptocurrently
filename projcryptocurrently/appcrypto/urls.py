57f14c540ef733e197c5ee5c9ad9a6fa57f2b360
kendra.go <kendra.go@obf.ateneo.edu>
2022-12-10 04:39:14 +0800

    added env and fixed homepageview urls

--- projcryptocurrently/appcrypto/urls.py
+++ projcryptocurrently/appcrypto/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import homepageview, coinview, reachview
 
 urlpatterns = [
-    path('', homepageview, name='homepagaview'),
+    path('', homepageview, name='homepageview'),
     path('coin/<coin_name>', coinview, name='coinview'),
     path('reach', reachview, name='reachview'),
 ]
